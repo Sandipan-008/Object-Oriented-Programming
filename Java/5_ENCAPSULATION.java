a26330e54f00aa609804cd3bad027640e34e1d19
Sandipan Majumder <sandmaj2001@gmail.com>
2024-10-16 22:21:27 +0530

    Update 5_ENCAPSULATION.java

--- Java/5_ENCAPSULATION.java
+++ Java/5_ENCAPSULATION.java
@@ -1,1 +1,23 @@
 
+class Account {
+    public String name;
+    protected String email;
+    private String password;
+
+    public String getpassword(){
+        return this.password;
+    }
+    public void setpassword(String Pass){
+        this.password = Pass;
+    }
+ 
+}
+ public class bank{
+     public static void main(String args[]){
+         Account account1 = new Account();
+         account1.name = "apna college";
+         account1.email = "apnacollege@gmail.com";
+         account1.setpassword("abcd");
+         System.out.print(account1.getpassword());
+     }
+ }

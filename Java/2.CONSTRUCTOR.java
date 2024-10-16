bb5f3b8d86511f005b22e68a878a3b4a8e77c514
Sandipan Majumder <sandmaj2001@gmail.com>
2024-10-16 20:41:58 +0530

    Update 2.CONSTRUCTOR.java

--- Java/2.CONSTRUCTOR.java
+++ Java/2.CONSTRUCTOR.java
@@ -33,17 +33,42 @@ class student{
     student(String name, int age){
      this.name=name;
      this.age=age;
-    }
-    
-}
-    
+    }    
+}    
 public class std{
     public static void main(String[]args){
         student s1 = new student("sandipan",22);
         s1.info();
     }
 }  
-
+_________________________________________________________________________________________
+class dog{
+    String name;
+    int age;
+    String color;
+    String breed;
+    
+    public void does(){
+        System.out.println(this.name + " Play,eat & sleep " + " with me");
+    }
+    public void info() {
+    System.out.println(String.format("%s is a %d years old %s %s dog", this.name, this.age, this.color, this.breed));
+} 
+    dog (String name, int age, String color, String breed){
+        this.name = name;
+        this.age = age;
+        this.color = color;
+        this.breed = breed;
+    }
+}
+public class oop{
+    public static void main(String[]args){
+        dog Mydog = new dog("tubi", 4, "white", "pomeranian");
+        Mydog.does();
+        Mydog.info();
+        
+    }
+}
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>...
 // constructor 3 [copy constructor]
 class student{
